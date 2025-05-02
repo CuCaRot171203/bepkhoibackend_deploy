82e5ccd61a65d7650be55cf6d507e90aecb440ed
CuCaRot171203 <tungnguyenthanh710@gmail.com>
2025-05-02 18:02:15 +0700

    code commit fix print pdf

--- BepKhoiBackend.BusinessObject/obj/Debug/net6.0/BepKhoiBackend.BusinessObject.AssemblyInfo.cs
+++ BepKhoiBackend.BusinessObject/obj/Debug/net6.0/BepKhoiBackend.BusinessObject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BepKhoiBackend.BusinessObject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8e0054ea9f8829290deff1597ab9207ee4a4803")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5611e3839e3c6e2ff3280e7106e2bd2e15d5a5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BepKhoiBackend.BusinessObject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BepKhoiBackend.BusinessObject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
