569d191c8ed6f482985d2c7497cda763b13a54a3
CuCaRot171203 <tungnguyenthanh710@gmail.com>
2025-05-02 18:19:02 +0700

    code commit fix print

--- BepKhoiBackend.API/obj/Debug/net6.0/BepKhoiBackend.API.AssemblyInfo.cs
+++ BepKhoiBackend.API/obj/Debug/net6.0/BepKhoiBackend.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BepKhoiBackend.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5611e3839e3c6e2ff3280e7106e2bd2e15d5a5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82e5ccd61a65d7650be55cf6d507e90aecb440ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("BepKhoiBackend.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BepKhoiBackend.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
